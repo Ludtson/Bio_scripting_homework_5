9d4bb6e37084374a0c403777223ab314caf10d36
Adekola Owoyemi <oluwadekolatosin@gmail.com>
2020-02-25 13:00:42 -0600

    commented out all part ot the script to look thru it line by line and run in shell as I edit

--- part_1/fix_me.sh
+++ part_1/fix_me.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 
 # Reading data from the user's input
+echo
+echo
+echo This script performs basic math operations on user numeric input...
+echo
 echo 'Enter a : '
 read a
-echo 'Enter b : ' 
+echo
+echo 'Enter b : '
 read b
 
 add=$((a + b))
 
+echo
 echo Addition of a and b are $add
+echo
 
-sub=$((a - b))
-echo Subtraction of a and b are $sub
+#sub=$((a - b))
+#echo Subtraction of a and b are $sub
 
-mul=$((a * b))
-echo Multiplication of a and b are $mul
+#mul=$((a * b))
+#echo Multiplication of a and b are $mul
 
-div=$((a / b))
-echo division of a and b are $div
+#div=$((a / b))
+#echo division of a and b are $div
 
-mod=$((a % b))
-echo Modulus of a and b are $mod
+#mod=$((a % b))
+#echo Modulus of a and b are $mod
 
-((++a))
-echo Increment operator when applied on "a" results into a = $a
+#((++a))
+#echo Increment operator when applied on "a" results into a = $a
 
-((--b))
-echo Decrement operator when applied on "b" results into b = $b
+#((--b))
+#echo Decrement operator when applied on "b" results into b = $b
 
-((--c))
-echo What was the default value \ of c if its value is now $c\?
+#((--c))
+#echo What was the default value \ of c if its value is now $c\?
