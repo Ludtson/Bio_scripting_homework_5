f7006602890a972fc78548baac56ae14a888467d
Adekola Owoyemi <oluwadekolatosin@gmail.com>
2020-02-25 21:01:06 -0600

    created new folder for part 2 of assignment and started writing code

---
+++ sys_info_OWOYEMI.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#This shell script was written with help from the internet by Adekola Owoyemi
+# called sys_info which output the following:
+# + Your username
+# + The time and date
+# + Who is logged on
+# + separated by a line of asterices (*******) after each section
+#and commits these data to file named ${user}_sysinfo.txt
+
+########################################################################
+#a handy function for the line of asterices
+
+function aste__ks(){
+ echo "*************************************************"
+}
+########################################################################
+echo "Your username : $(echo $USER)"
+aste__ks # call function
+
