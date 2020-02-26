f9d5844b86cbfaeaa134533cdef91d9b66297b0a
Adekola Owoyemi <oluwadekolatosin@gmail.com>
2020-02-25 21:25:40 -0600

    finished writing all line of code with output on STDOUT

--- part_2/sys_info_OWOYEMI.sh
+++ part_2/sys_info_OWOYEMI.sh
@@ -11,11 +11,28 @@
 ########################################################################
 #a handy function for the line of asterices
 
-function aste__ks(){
+function aste__ks(){ #funny name to use for a function
  echo "*************************************************"
 }
 ########################################################################
+#username commands
 echo
 echo "Your username : $(echo $USER)"
 aste__ks # call line of asterices function
+#######################################################################
+#date and time commands
+echo
+echo "Current date and time : $(date)" #BOOM! a simple date command
+aste__ks
+#######################################################################
+#current logged on users commands
+echo
+echo "Currently logged on users:"
+who #who did it?
+aste__ks
+
+
+echo "         *******************************            "
+echo
+echo
 
